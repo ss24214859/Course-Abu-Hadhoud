b9c7984604d826f37551d77445f43814d964d124
Hamo Shaaban <ss24214859@gmail.com>
2025-03-13 06:01:52 +0200

    Abu-Hadhoud Sloution

--- 8_Algorithms_level_4/1.cpp
+++ 8_Algorithms_level_4/1.cpp
@@ -6,87 +6,63 @@
 #include <iomanip>
 using namespace std;
 
-
-short DigitCounter(long int Number)
+int ReadPositiveNumber(string massge)
 {
-    short counter = 0;
-    while (Number != 0)
+    int num;
+    do
     {
-        Number /= 10;
-        counter++;
-    }
-    return counter;
+    cout<<massge;
+    cin>>num;
+    } while (num<0);
+
+    return num;
 }
 
-string TextOfNumber(long int Number)
+
+string NumberToText(long int Number)
 {
-    string Singles[19] = { "one", "two", "three", "four", "five", "six", "seven", "eight",
+    string Singles[20] = { "","one", "two", "three", "four", "five", "six", "seven", "eight",
         "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
-         "seventeen", "eighteen", "nineteen" };
-
-    string tens[8] = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-
-    short NumberOfDigits = DigitCounter(Number) - 1;
-
-
-    string SinglesOf = "";
-    /*for(int i=1.1;i<20;i++)                    //test Arr
-        cout<<Singles[i-1]<<endl;*/
-
-    string Text = "";
+         "seventeen", "eighteen", "nineteen" }; 
 
     if (Number == 0)
-        Text = "Zero";
+    return "";
     else if (Number > 0 && Number <= 19)
     {
-        return Singles[Number - 1];     
+        return Singles[Number];     
     }
     else if (Number >= 20 && Number <= 90)
     {
-        Text = tens[(Number / 10) - 2];
-        
+        string tens[10] = { "","","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        return tens[Number / 10] + " " + NumberToText(Number % 10);
     }
     else if (Number >= 100 && Number <= 900)
     {
-        SinglesOf = Singles[(Number / 100) - 1];
-        Text = SinglesOf + ((SinglesOf == "one") ? " Hunderd" : " Hunderds");
+        string NumberOfHunderds= NumberToText(Number/100);
+        return NumberOfHunderds + ((NumberOfHunderds == "one") ? " Hunderd" : " Hunderds,") + NumberToText(Number % 100);
     }
     else if (Number >= 1000 && Number <= 900000)
     {
-        string NumberOfThousand = TextOfNumber(Number / 1000);
-
-        Text = NumberOfThousand + ((NumberOfThousand == "one") ? " Thousand" : " Thousands");
-        NumberOfDigits = DigitCounter(Number) -DigitCounter(Number/1000);
+        string NumberOfThousand = NumberToText(Number / 1000);
+        return NumberOfThousand + ((NumberOfThousand == "one") ? " Thousand" : " Thousands,") + NumberToText(Number % 1000);
     }
     else if (Number >= 1000000 && Number <= 900000000)
     {
-        string NumberOfMilion = TextOfNumber(Number / 1000000);
+        string NumberOfMilion = NumberToText(Number / 1000000);
 
-        Text = NumberOfMilion + ((NumberOfMilion == "one") ? " Milion" : " Milions");
-        NumberOfDigits = DigitCounter(Number) - DigitCounter(Number/1000000);
+        return NumberOfMilion + ((NumberOfMilion == "one") ? " Milion" : " Milions,")+NumberToText(Number%1000000);
     }
     else if (Number >= 1000000000 && Number <= 900000000000)
     {
-        string NumberOfBilion = TextOfNumber(Number / 1000000000);
+        string NumberOfBilion = NumberToText(Number / 1000000000);
 
-        Text = NumberOfBilion + ((NumberOfBilion == "one") ? " Bilion" : " Bilions");
-        NumberOfDigits = DigitCounter(Number) - DigitCounter(Number/1000000000);
+        return NumberOfBilion + ((NumberOfBilion == "one") ? " Bilion" : " Bilions,") + NumberToText(Number%1000000000);    
     }
-
-
-
-
-    if (NumberOfDigits == 0)      //shutdown recergine.
-        return Text;
-
-    long int PowerOf10WithDigit = long (pow(10, NumberOfDigits));
-
-    Number/*=855*/ %= PowerOf10WithDigit; //to remove first Number on the left.example:{ Number =855 after-> =55 } 
-
-    return Text + " " + TextOfNumber(Number/*=55*/);
+    return "";
 }
 
 int main()
 {
-    cout << TextOfNumber(8476362741);
+    
+    cout << NumberToText(ReadPositiveNumber("Enter Number ?"));
 }
