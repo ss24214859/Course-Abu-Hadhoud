d0262097bfafd972dd6ab162a2c7c478177244cb
Hamo Shaaban <ss24214859@gmail.com>
2025-03-17 07:37:31 +0200

    Abou-Hadhoud.

--- 8_Algorithms_level_4/5.cpp
+++ 8_Algorithms_level_4/5.cpp
@@ -6,86 +6,71 @@
 #include <iomanip>
 using namespace std;
 
-enum enMonths
-{
-    January = 1, February = 2, March = 3, April = 4,
-    May = 5, June = 6, July = 7, August = 8, September = 9,
-    October = 10, November = 11, December = 12
-};
-
 int ReadPositiveNumber(string massge)
 {
     int num;
     do
     {
-    cout<<massge;
-    cin>>num;
-    } while (num<0);
+        cout << massge;
+        cin >> num;
+    } while (num < 0);
 
     return num;
 }
 
+
+short ReadMonth(string Massage)
+{
+    short Month=0;
+    cout<<Massage;
+    cin>>Month;
+    return Month;
+}
+
 bool IsleapYear(int Month)
 {
-    return ( Month % 400 == 0 ) || ( Month % 100 != 0  &&  Month % 4 == 0 ) ;
+    return (Month % 400 == 0) || (Month % 100 != 0 && Month % 4 == 0);
 }
 
-int DaysInMonth(int Month , int Year)
+short DaysInMonth(short Month, int Year)
 {
-    switch(Month)
+    if(Month<1 ||Month>12)
+    return 0;
+
+    if(Month == 2)
+    return (IsleapYear(Year))? 29:28;
+
+    short arr31Days[7]={1,3,5,7,8,10,12};
+    for(short m : arr31Days)
     {
-        case enMonths::January :
+        if(m==Month)
         return 31;
-    case enMonths::February :
-        return IsleapYear(Year)? 29 : 28;
-    case enMonths::March :
-        return 31;
-    case enMonths::April :
-        return 30;
-    case enMonths::May :
-        return 31;
-    case enMonths::June :
-        return 30;
-    case enMonths::July :
-        return 31;
-    case enMonths::August :
-        return 31;
-    case enMonths::September :
-        return 30;
-    case enMonths::October :
-        return 31;
-    case enMonths::November :
-        return 30;
-    case enMonths::December :
-        return 31;
-    default:
-        return 0; 
-}
-    
+    }
+    return 30;  
 }
 
-int HoursInMonth(int Month , int Year)
+int HoursInMonth(short Month, int Year)
 {
-    return DaysInMonth(Month,Year)*24;
+    return DaysInMonth(Month, Year) * 24;
 }
 
-int MinutesInMonth(int Month , int Year)
+int MinutesInMonth(short Month, int Year)
 {
-    return HoursInMonth(Month,Year)*60;
+    return HoursInMonth(Month, Year) * 60;
 }
 
-int secondsInMonth(int Month , int Year)
+int secondsInMonth(short Month, int Year)
 {
-    return MinutesInMonth(Month,Year)*60;
+    return MinutesInMonth(Month, Year) * 60;
 }
 
 int main()
 {
-    int Year=ReadPositiveNumber("Enter a Year to Chack ?");
-    int Month=ReadPositiveNumber("Enter a Month to Chack ?");
-    
-    cout<< "Number Of Days    in Month ["<<Month<<"] is "<<DaysInMonth(Month,Year)<<endl;
-    cout<< "Number Of Hours   in Month ["<<Month<<"] is "<<HoursInMonth(Month,Year)<<endl;
-    cout<< "Number Of Minutes in Month ["<<Month<<"] is "<<MinutesInMonth(Month,Year)<<endl;
-    cout<< "Number Of Seconds in Month ["<<Month<<"] is "<<secondsInMonth(Month,Year)<<endl;
+    int Year = ReadPositiveNumber("Enter a Year to Chack ?");
+    short Month = ReadMonth("Enter a Month to Chack ?");
+
+    cout << "Number Of Days    in Month [" << Month << "] is " << DaysInMonth(Month, Year) << endl;
+    cout << "Number Of Hours   in Month [" << Month << "] is " << HoursInMonth(Month, Year) << endl;
+    cout << "Number Of Minutes in Month [" << Month << "] is " << MinutesInMonth(Month, Year) << endl;
+    cout << "Number Of Seconds in Month [" << Month << "] is " << secondsInMonth(Month, Year) << endl;
 }
