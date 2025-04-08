e43195926ffb60e20eb34b41f158f22de8cf85dd
Hamo Shaaban <ss24214859@gmail.com>
2025-04-08 08:56:28 +0200

    My Sl ' Chack Is Last Day and Is Last Month '

---
+++ 8_Algorithms_level_4/15.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+short ReadPositiveNumber(string massge)
+{
+    short num;
+    do
+    {
+        cout << massge;
+        cin >> num;
+    } while (num < 0);
+
+    return num;
+}
+
+bool IsleapYear(short Month)
+{
+    return (Month % 400 == 0) || (Month % 100 != 0 && Month % 4 == 0);
+}
+
+short DaysInMonth(short Month ,short Year )
+{
+    if(Month<1 ||Month>12)
+    return 0;
+
+    short arrDaysOfMonths[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    return (Month == 2 )? ( (IsleapYear(Year))? 29 : 28 ) : arrDaysOfMonths[Month-1] ;
+}
+
+struct stDate
+{
+    short Day,
+    Month,
+    Year;
+};
+
+short TotalDaysFromBeginningYear(int Year,short Month,short Day)
+{
+    short TotalDays=Day;
+
+    for(short i=Month-1;i>=1;i--)
+        TotalDays=TotalDays+DaysInMonth(i,Year);
+
+    return TotalDays;
+}
+
+
+stDate ReadDate()
+{
+    stDate Date;
+    Date.Year = ReadPositiveNumber("Enter a Year ?");
+    Date.Month = ReadPositiveNumber("Enter a Month ?");
+    Date.Day = ReadPositiveNumber("Enter a Day ?");
+    return Date;
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+    return (DaysInMonth(Date.Month,Date.Year)==Date.Day);
+}
+
+bool IsLastMonthInYear(short Month)
+{
+    return (Month == 12 );
+}
+
+int main()
+{
+    cout<<"Enter Date :\n";
+    stDate Date=ReadDate();
+
+    cout<<( (IsLastDayInMonth(Date))? "Yes, Day is Last Day in Month." : "No, Day is Not Last Day in Month" )<<endl;
+    cout<<( (IsLastMonthInYear(Date.Month))? "Yes, Month is Last Month in Year." : "No, Month is Not Last Month in Year" )<<endl;
+}
