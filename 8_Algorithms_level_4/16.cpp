c06a30b0b764c474013f272a94fc994857749f33
Hamo Shaaban <ss24214859@gmail.com>
2025-04-08 09:32:13 +0200

    My SL ' Increase Date By One Day '

---
+++ 8_Algorithms_level_4/16.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+short ReadPositiveNumber(string massge)
+{
+    short num;
+    do
+    {
+        cout << massge;
+        cin >> num;
+    } while (num < 0);
+
+    return num;
+}
+
+bool IsleapYear(short Month)
+{
+    return (Month % 400 == 0) || (Month % 100 != 0 && Month % 4 == 0);
+}
+
+short DaysInMonth(short Month ,short Year )
+{
+    if(Month<1 ||Month>12)
+    return 0;
+
+    short arrDaysOfMonths[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    return (Month == 2 )? ( (IsleapYear(Year))? 29 : 28 ) : arrDaysOfMonths[Month-1] ;
+}
+
+struct stDate
+{
+    short Day,
+    Month,
+    Year;
+};
+
+short TotalDaysFromBeginningYear(int Year,short Month,short Day)
+{
+    short TotalDays=Day;
+
+    for(short i=Month-1;i>=1;i--)
+        TotalDays=TotalDays+DaysInMonth(i,Year);
+
+    return TotalDays;
+}
+
+
+stDate ReadDate()
+{
+    stDate Date;
+    Date.Year = ReadPositiveNumber("Enter a Year ?");
+    Date.Month = ReadPositiveNumber("Enter a Month ?");
+    Date.Day = ReadPositiveNumber("Enter a Day ?");
+    return Date;
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+    return (DaysInMonth(Date.Month,Date.Year)==Date.Day);
+}
+
+bool IsLastMonthInYear(short Month)
+{
+    return (Month == 12 );
+}
+
+stDate IncreaseDateByOneDay(stDate Date)
+{
+    if(IsLastDayInMonth(Date))
+    {
+        if(IsLastMonthInYear(Date.Month))
+        {
+            Date.Year++;
+            Date.Month=1;
+        }
+        else
+        {
+            Date.Month++;
+        }
+
+        Date.Day=1;
+    }
+    else
+    {
+        Date.Day++;
+    }
+
+    return Date;
+}
+
+int main()
+{
+    cout<<"Enter Date :\n";
+    stDate Date=ReadDate();
+
+    Date=IncreaseDateByOneDay(Date);
+
+    printf("Date After Adding one Day is : %d/%d/%d " , Date.Day,Date.Month,Date.Year);
+}
