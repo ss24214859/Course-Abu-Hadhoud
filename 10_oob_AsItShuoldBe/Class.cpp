0cff52d52bf61715765282399f59c6e72e2a5732
Hamo Shaaban <ss24214859@gmail.com>
2025-05-09 18:15:46 +0300

    Property Get,set Through "="

--- 10_oob_AsItShuoldBe/Class.cpp
+++ 10_oob_AsItShuoldBe/Class.cpp
@@ -7,52 +7,33 @@ class clsPerson
 {
 
 private:
-    int _ID = 2022;
     string _FirstName;
     string _LastName;
-    vector<string> _HestoryOfFirstName;
-    vector<string> _HestoryOfLastName;
 
 public:
     void SetFirstName(string NewFirstName)
     {
-        _HestoryOfFirstName.push_back(_FirstName);
         _FirstName = NewFirstName;
     }
-    void SetLastName(string NewLastName)
-    {
-        _HestoryOfLastName.push_back(_LastName);
-        _LastName = NewLastName;
-    }
-    string FirstName()
+
+    string GetFirstName()
     {
         return _FirstName;
     }
 
-    string LastName()
+    __declspec(property(get = GetFirstName, put = SetFirstName)) string FirstName;
+
+    void SetLastName(string NewLastName)
+    {
+        _LastName = NewLastName;
+    }
+
+    string GetLastName()
     {
         return _LastName;
     }
 
-    int ID()
-    {
-        return _ID;
-    }
-
-    string FullName()
-    {
-        return _FirstName + " " + _LastName;
-    }
-    void PrintHistoryOfFirstName()
-    {
-        cout << endl;
-        Vector::PrintVector(_HestoryOfFirstName);
-    }
-    void PrintHistoryOfLastName()
-    {
-        cout << endl;
-        Vector::PrintVector(_HestoryOfLastName);
-    }
+    __declspec(property(get = GetLastName, put = SetLastName)) string LastName;
 };
 
 int main()
@@ -60,36 +41,12 @@ int main()
     clsPerson Person1;
     Person1.SetFirstName("Hamo");
     Person1.SetLastName("Shaaban");
-    cout << "FullName" << Person1.FullName() << endl;
-    cout << "ID" << Person1.ID() << endl;
-    Person1.SetFirstName("doha");
-    Person1.SetLastName("Kardoushy");
-    cout << "FullName" << Person1.FullName() << endl;
-    cout << "ID" << Person1.ID() << endl;
-    Person1.SetFirstName("Shaimaa");
-    Person1.SetLastName("Mamoud");
-    cout << "FullName" << Person1.FullName() << endl;
-    cout << "ID" << Person1.ID() << endl;
-    Person1.SetFirstName("bakhit");
-    Person1.SetLastName("Kofta");
-    cout << "FullName" << Person1.FullName() << endl;
-    cout << "ID" << Person1.ID() << endl;
-    Person1.SetFirstName("Zaky");
-    Person1.SetLastName("Nader");
-    cout << "FullName" << Person1.FullName() << endl;
-    cout << "ID" << Person1.ID() << endl;
-    Person1.SetFirstName("homus");
-    Person1.SetLastName("Ali");
-    cout << "FullName" << Person1.FullName() << endl;
-    cout << "ID" << Person1.ID() << endl;
-    Person1.SetFirstName("homus");
-    Person1.SetLastName("Rady");
-    cout << "FullName" << Person1.FullName() << endl;
-    cout << "ID" << Person1.ID() << endl;
+    cout << "FullName :" << Person1.GetFirstName() << endl;
+    cout << "LastName :" << Person1.GetLastName() << endl;
 
-    cout << "History Of First Name Of Person1 : " << endl;
-    Person1.PrintHistoryOfFirstName();
-
-    cout << "History Of Last Name Of Person1 : " << endl;
-    Person1.PrintHistoryOfLastName();
+    // instead of The above, we only write this
+    Person1.FirstName = "Hamo";
+    Person1.LastName = "Shaaban";
+    cout << "FullName :" << Person1.FirstName << endl;
+    cout << "LastName :" << Person1.LastName << endl;
 }
