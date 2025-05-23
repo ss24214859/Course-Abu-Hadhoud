73eb4b5736b85cbc6ff7fa07d45612c705fb992a
Hamo Shaaban <ss24214859@gmail.com>
2025-05-23 11:02:39 +0300

    Constructors

--- 10_oob_AsItShuoldBe/Class.cpp
+++ 10_oob_AsItShuoldBe/Class.cpp
@@ -32,11 +32,72 @@ public:
     }
 };
 
+class clsAddress
+{
+private:
+    string _AddressLine1;
+    string _AddressLine2;
+    string _POBox;
+    string _ZipCode;
+
+public:
+    clsAddress(string AddressLine1, string AddressLine2, string POBox, string ZipCode)
+    {
+        _AddressLine1 = AddressLine1;
+        _AddressLine2 = AddressLine2;
+        _POBox = POBox;
+        _ZipCode = ZipCode;
+    }
+
+    void setAddressLine1(string AddressLine1)
+    {
+        _AddressLine1 = AddressLine1;
+    }
+    string AddressLine1()
+    {
+        return _AddressLine1;
+    }
+
+    void setAddressLine2(string AddressLine2)
+    {
+        _AddressLine2 = AddressLine2;
+    }
+    string AddressLine2()
+    {
+        return _AddressLine2;
+    }
+
+    void setPOBox(string POBox)
+    {
+        _POBox = POBox;
+    }
+    string POBox()
+    {
+        return _POBox;
+    }
+
+    void setZipCode(string ZipCode)
+    {
+        _ZipCode = ZipCode;
+    }
+    string ZipCode()
+    {
+        return _ZipCode;
+    }
+
+    void Print()
+    {
+        cout << "Address Line 1 : " << _AddressLine1 << endl;
+        cout << "Address Line 2 : " << _AddressLine2 << endl;
+        cout << "PO Box         : " << _POBox << endl;
+        cout << "Zip Code       : " << _ZipCode << endl;
+    }
+};
+
 int main()
 {
-    clsPerson Person1;
-    Person1.SetFirstName("Hamo");
-    Person1.SetLastName("Shaaban");
-    cout << "FullName :" << Person1.GetFirstName() << endl;
-    cout << "LastName :" << Person1.GetLastName() << endl;
+    clsAddress Address1("ALdikhla Alex", "Al Shamhadan", "ha898", "001545");
+    Address1.Print();
+
+    system("pause>0");
 }
