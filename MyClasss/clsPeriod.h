beaeee70dac31aedb886a020d432a8bb61ece0df
Hamo Shaaban <ss24214859@gmail.com>
2025-05-30 22:27:24 +0300

    Period Class

---
+++ MyClasss/clsPeriod.h
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "../MyClasss/clsDate.h"
+
+class clsPeriod
+{
+private:
+    clsDate _StartDate;
+    clsDate _EndDate;
+
+public:
+    clsPeriod(clsDate StartDate, clsDate EndDate)
+    {
+        _StartDate = StartDate;
+        _EndDate = EndDate;
+    }
+
+    static void Print(clsPeriod Period)
+    {
+        cout << "Start Date : " << clsDate::DateToString(Period._StartDate) << endl;
+        cout << "End Date   : " << clsDate::DateToString(Period._EndDate) << endl;
+    }
+    void Print()
+    {
+        Print(*this);
+    }
+
+    static bool IsOverLapPeriods(clsPeriod Period1, clsPeriod Period2)
+    {
+
+        return !((clsDate::CompareDates(Period1._EndDate, Period2._StartDate) == clsDate::enDateCompare::Before) ||
+                 (clsDate::CompareDates(Period1._StartDate, Period2._EndDate) == clsDate::enDateCompare::After));
+    }
+    bool IsOverLapPeriods(clsPeriod Period2)
+    {
+        return IsOverLapPeriods(*this, Period2);
+    }
+
+    static int PeriodLength(clsPeriod Period, bool IncludingEnd_Day = false)
+    {
+        return clsDate::GetDifferenceInDays(Period._StartDate, Period._EndDate, IncludingEnd_Day);
+    }
+    int PeriodLength(bool IncludingEndDay = false)
+    {
+        return PeriodLength(*this, IncludingEndDay);
+    }
+
+    static bool IsDateInPeriod(clsPeriod Period, clsDate Date)
+    {
+        return !((clsDate::CompareDates(Date, Period._StartDate) == clsDate::enDateCompare::Before) ||
+                 (clsDate::CompareDates(Date, Period._EndDate) == clsDate::enDateCompare::After));
+    }
+    bool IsDateInPeriod(clsDate Date)
+    {
+        return IsDateInPeriod(*this, Date);
+    }
+
+    static void SwapPeriods(clsPeriod &Period1, clsPeriod &Period2)
+    {
+        clsPeriod temp = Period2;
+        Period2 = Period1;
+        Period1 = temp;
+    }
+
+    static int OverlapDaysCounter(clsPeriod Period1, clsPeriod Period2)
+    {
+        clsPeriod OverLapPeriod((clsDate::IsDate1BeforeDate2(Period1._StartDate, Period2._StartDate)) ? Period2._StartDate : Period1._StartDate, (clsDate::IsDate1BeforeDate2(Period1._EndDate, Period2._EndDate)) ? Period1._EndDate : Period2._EndDate);
+        if (clsDate::IsDate1BeforeDate2(OverLapPeriod._EndDate, OverLapPeriod._StartDate))
+            return 0;
+
+        return PeriodLength(OverLapPeriod);
+    }
+    int OverlapDaysCounter(clsPeriod Period2)
+    {
+        return OverlapDaysCounter(*this, Period2);
+    }
+};
---
+++ MyClasss/clsRead.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class clsRead
+{
+public:
+    static string ReadLine(string Massage = "")
+    {
+        string Line;
+        cout << Massage;
+        getline(cin >> ws, Line);
+        return Line;
+    }
+
+    static string ReadString(string Massage = "")
+    {
+        string String;
+        cout << Massage;
+        cin >> String;
+        return String;
+    }
+
+    static int ReadNumber(string Message = "")
+    {
+        int Number = 0;
+        do
+        {
+            cout << Message;
+            cin >> Number;
+            if (cin.fail())
+            {
+                cout << "Error! This is Not Number.Try again";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+            else
+                break;
+
+        } while (true);
+
+        return Number;
+    }
+
+    static int ReadPosNum(string Message = "")
+    {
+        int Number = 0;
+        do
+        {
+            Number = ReadNumber(Message);
+        } while (Number <= 0);
+
+        return Number;
+    }
+
+    static int ReadNumberInRang(int Min, int Max, string Message, string ErrorMessage = "Invalid Number")
+    {
+        int Number = 0;
+        do
+        {
+            Number = ReadNumber(Message);
+            if (Number < Min || Number > Max)
+                cout << ErrorMessage;
+        } while (Number < Min || Number > Max);
+
+        return Number;
+    }
+
+    static bool ReadYesOrNo(string massage)
+    {
+        char c = 'h';
+        while (c != 'y' && c != 'n' && c != 'Y' && c != 'N')
+        {
+            cout << massage;
+            cin >> c;
+
+            if (c == 'y' || c == 'Y')
+                return true;
+            else if (c == 'n' || c == 'N')
+                return false;
+        }
+        return 0;
+    }
+
+    static int ReadNumberMultipleOfX(int X, string Massage = "")
+    {
+        int Number = 0;
+        do
+        {
+            if (Massage != "")
+                cout << Massage << X << "'s ? ";
+
+            Number = ReadPosNum();
+        } while (!(Number % X == 0));
+        return Number;
+    }
+};
