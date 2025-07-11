57d76066be0935d53f8d82913a0512b5a58d2353
Hamo Shaaban <ss24214859@gmail.com>
2025-07-11 07:51:11 +0300

    Update Currenccy Rate Screen

--- 11_oob_Applications/Project3 Bank/clsUpdateRateScreen.h
+++ 11_oob_Applications/Project3 Bank/clsUpdateRateScreen.h
@@ -5,14 +5,45 @@
 #include <string>
 #include "clsCurrency.h"
 #include "../../MyClasss/clsInputValidate.h"
+#include "clsCurrencyUtil.h"
 
 class clsUpdateRateScreen : protected clsScreen
 {
+    static void _UpdateRate(clsCurrency &Currency)
+    {
+            if(clsInputValidate::ReadYesOrNo("Are you sure you want to update the rate? (Y/N)"))
+            {
+                cout<<"\nUpdate Currency Rate\n";
+                cout<<"_______________________________\n";
+                float NewRate = clsInputValidate::ReadPosFloat( "Enter New Rate: ");
+                if(Currency.UpdateRate(NewRate) == clsCurrency::enSaveResult::svSucceeded)
+                {
+                    cout<<"\nCurrency Rate Updated Successfully :-) \n";
+                    clsCurrencyUtil::PrintCurrency(Currency);
+
+                }
+                else
+                {
+                    cout<<"\nFailed to Update Currency Rate.\n";
+                }
+            }
+
+    }
 
 public:
     static void ShowUpdateRateScreen()
     {
-        cout<<"Update Rate Screen Will be Here."<<endl;
+        _DrawScreenHeader("\t\tUpdate Currency Screen");
+       clsCurrency Currency = clsCurrency::FindByCurrencyCode( clsInputValidate::ReadString("Enter Currency Code: "));
+       if(!Currency.IsEmpty())
+       {
+            clsCurrencyUtil::PrintCurrency(Currency);
+            _UpdateRate(Currency);
+       }
+       else
+       {
+            cout<<"\nCurrency Not Found.\n";
+       }
     }
 
 }; 
