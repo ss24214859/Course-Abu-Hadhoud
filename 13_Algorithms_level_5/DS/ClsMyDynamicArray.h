5db36a051addc8de004cddf39bddb881d8d204f4
Hamo Shaaban <ss24214859@gmail.com>
2025-08-04 13:18:32 +0300

    Project 4 My Array Abu Hadhoud Slution.

--- 13_Algorithms_level_5/DS/ClsMyDynamicArray.h
+++ 13_Algorithms_level_5/DS/ClsMyDynamicArray.h
@@ -1,24 +1,59 @@
 #pragma once
 #include <iostream>
-#include "ClsDblLinkedList.h"
+using namespace std;
 
 template <class T>
-class ClsMyDynamicArray : public ClsDblLinkedList<T>
+class ClsMyDynamicArray
 {
-
+protected:
+    int _Size;
 
 public:
-    ClsMyDynamicArray(int size)
+    T* OriginalArray;
+
+    ClsMyDynamicArray(int Size)
     {
-        T EmptyValue;
-        while(size!=this->_Size)
-        {
-            this->InsertAtEnd(EmptyValue);
-        }
+        if(Size<0)
+            _Size=0;
+    
+        _Size=Size;
+
+        OriginalArray= new T [_Size];
+    }
+    ~ClsMyDynamicArray()
+    {
+        delete[] OriginalArray;
     }
 
-    void SetItem(int Index,T Value)
+    bool SetItem(int Index,T Value)
     {
-        this->UpdateItem(Index,Value);
+        if(Index < _Size && (Index>=0))
+        {
+            OriginalArray[Index]=Value;
+            return true;
+        }
+        else
+        return false;
     }
+
+    int Size()
+    {
+        return _Size;
+    }
+
+    bool IsEmpty()
+    {
+        return _Size==0;
+    }
+
+    void PrintList()
+    {
+        for(int i=0;i<_Size;i++)
+        {
+            cout<<OriginalArray[i]<<" ";
+        }
+        cout<<"\n";
+    }
+    
+    
 };
--- 13_Algorithms_level_5/MainProject_4_MyArray.cpp
+++ 13_Algorithms_level_5/MainProject_4_MyArray.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    ClsMyDynamicArray <int> MyArray(3);
+    ClsMyDynamicArray <int> MyArray(5);
 
     MyArray.SetItem(0,5);
     MyArray.SetItem(1,10);
@@ -13,7 +13,7 @@ int main()
     cout<<"\nArray Size : "<<MyArray.Size()<<endl;
     cout<<"IsEmpty  :" << MyArray.IsEmpty()<<endl;
     cout<<"Array Items: "<<endl;
-    MyArray.Print();
+    MyArray.PrintList();
 
 
 }
