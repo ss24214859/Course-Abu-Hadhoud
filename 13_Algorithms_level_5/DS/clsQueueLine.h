dbeafc7eee9ec04774dd4bf908d9ef3a6fa08ecd
Hamo Shaaban <ss24214859@gmail.com>
2025-08-09 20:37:34 +0300

    Project 8 Abu Hadoud Sloution

--- 13_Algorithms_level_5/DS/clsQueueLine.h
+++ 13_Algorithms_level_5/DS/clsQueueLine.h
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 #include <iostream>
 #include "../../MyClasss/clsTime.h"
 #include "../../MyClasss/clsDate.h"
@@ -10,132 +10,219 @@ class clsQueueLine
 {
 
 private:
-        struct TicketInfo
-        {
-            string PrefixNumber;
-            clsTime TicketTime;
-            clsDate TicketDate;
-            short WatingClients;
-            string ServeTime;
-        };
-        string _Taps="\t\t\t\t\t";
-
+    class clsTicket
+    {
         string _Prefix;
-        int _ServeTime;
-        queue<TicketInfo> _TotalTickets;
-        short _ServedClients=0;
-        short _WatingClients=0;
-protected:
-        template <typename T>
-        stack <T> _ConvertFromQueueToStack(queue<T> queue)
-        {
-            stack <T> stack;
-            while(!queue.empty())
-            {
-                stack.push(queue.front());
-                queue.pop();
-            }
-            return stack;
-        }
+        short _Number;
+        string _TicketDateTime;
+        short _WatingClients;
+        short _AverageServeTime;
+        string _ExpectedServeTime;
 
         string _TimeFromMinutesTostring(int Minutes)
         {
-            if(Minutes<60)
-            return to_string(Minutes)+" Minutes";
+            if (Minutes < 60)
+                return to_string(Minutes) + " Minutes";
 
-            short hours=0;
-            while(Minutes>=60)
+            short hours = 0;
+            while (Minutes >= 60)
             {
                 hours++;
-                Minutes-=60;
+                Minutes -= 60;
             }
-            if(Minutes==0)
-            return to_string(hours)+" Hours";
+            if (Minutes == 0)
+                return to_string(hours) + " Hours";
             else
-            return to_string(hours)+" Hours And " + to_string(Minutes)+" Minutes";
+                return to_string(hours) + " Hours And " + to_string(Minutes) + " Minutes";
         }
+
+    public:
+        clsTicket(string Prefix, short Number, short WatingClients, short AvreageServeTime)
+        {
+            _Prefix = Prefix;
+            _Number = Number;
+            _TicketDateTime = clsDate::GetSystemDateTimeString();
+            _WatingClients = WatingClients;
+            _AverageServeTime = AvreageServeTime;
+            _ExpectedServeTime = _TimeFromMinutesTostring(AvreageServeTime * _WatingClients);
+        }
+
+        string GetPrefix() const
+        {
+            return _Prefix;
+        }
+
+        short GetNumber() const
+        {
+            return _Number;
+        }
+
+        clsDate GetTicketDateTime() const
+        {
+            return _TicketDateTime;
+        }
+
+        short GetWatingClients() const
+        {
+            return _WatingClients;
+        }
+
+        short GetAvreageServeTime() const
+        {
+            return _AverageServeTime;
+        }
+
+        string GetExpectedServeTime() const
+        {
+            return _ExpectedServeTime;
+        }
+
+        string FullNumber()
+        {
+            return _Prefix + to_string(_Number);
+        }
+
+        void Print()
+        {
+            cout <<     "\n\t\t\t\t______________________________\n\n";
+            cout << "\t\t\t\t"<<"             "<<FullNumber()<<"\n\n";
+            cout << "\t\t\t\t"<<" "<<_TicketDateTime<<endl;
+            cout << "\t\t\t\t"<<" Wating Clients = "<<_WatingClients<<endl;
+            cout << "\t\t\t\t"<<"    Serve Time In "<<endl;
+            cout << "\t\t\t\t"<<"   "<<_ExpectedServeTime<<"."<<endl;
+            cout <<       "\t\t\t\t______________________________\n"<<endl;
+        }
+
+    };
+
+    string _Prefix="";
+    int _AverageServeTime=0;
+    short _TotalTickets=0;
+
+protected:
+    template <typename T>
+    stack<T> _ConvertFromQueueToStack(queue<T> queue)
+    {
+        stack<T> stack;
+        while (!queue.empty())
+        {
+            stack.push(queue.front());
+            queue.pop();
+        }
+        return stack;
+    }
+
 public:
-        clsQueueLine(string Prefix , int ServeTime)
+    queue <clsTicket> QueueLine;
+
+    clsQueueLine(string Prefix, int AverageServeTime)
+    {
+        _Prefix = Prefix;
+        _AverageServeTime = AverageServeTime;
+    }
+
+    void IssueTicket()
+    {
+        _TotalTickets++;
+        QueueLine.push(clsTicket(_Prefix,_TotalTickets,WatingClients(),_AverageServeTime));
+    }
+
+    short WatingClients()
+    {
+        return QueueLine.size();
+    }
+
+    short ServedClients()
+    {
+        return _TotalTickets - WatingClients(); 
+    }
+
+    string WhoIsNext()
+    {
+        if(QueueLine.empty())
+        return "No Clients Left.";
+        else
+        return QueueLine.front().FullNumber();
+    }
+
+    void PrintInfo()
+    {
+
+        cout << "\t\t\t\t__________________________________\n"
+             << endl;
+        cout << "\t\t\t\t            Queue Info            " << endl;
+        cout << "\t\t\t\t__________________________________\n"
+             << endl;
+        cout << "\t\t\t\t" << "Prefix = " << _Prefix << endl;
+        cout << "\t\t\t\t" << "Total Tickets = " << _TotalTickets << endl;
+        cout << "\t\t\t\t" << "Served Clients = " << ServedClients() << endl;
+        cout << "\t\t\t\t" << "Wating Clients = " << WatingClients() << endl;
+        cout << "\t\t\t\t__________________________________\n"
+             << endl;
+    }
+
+    void PrintTicketsLineRTL()
+    {
+        cout << "\t\tTickets :  ";
+        
+        if(QueueLine.empty())
         {
-            _Prefix=Prefix;
-            _ServeTime=ServeTime;
+            cout<<"No Clients Left."<<endl;
+            return;
         }
 
-        void IssueTicket()
+        queue<clsTicket> _tempTotalTickets = QueueLine;
+        while (!_tempTotalTickets.empty())
         {
-            TicketInfo NewTicket;
-            NewTicket.PrefixNumber=_Prefix + to_string(_TotalTickets.size()+1);
-            NewTicket.TicketDate.GetSystemDate();
-            NewTicket.TicketTime.GetSystemTime();
-            NewTicket.WatingClients=_WatingClients;
-            NewTicket.ServeTime=_TimeFromMinutesTostring(_ServeTime*NewTicket.WatingClients);
-            _TotalTickets.push(NewTicket);
-            _WatingClients++;
+            cout << _tempTotalTickets.front().FullNumber() << " <-- ";
+            _tempTotalTickets.pop();
+        }
+        cout << endl;
+    }
 
+    void PrintTicketsLineLTR()
+    {
+        cout << "\t\tTickets :  ";
+        if(QueueLine.empty())
+        {
+            cout<<"No Clients Left."<<endl;
+            return;
+        }
+
+        stack<clsTicket> _StackTotalTickets = _ConvertFromQueueToStack(QueueLine);
+        while (!_StackTotalTickets.empty())
+        {
+            cout << _StackTotalTickets.top().FullNumber() << " --> ";
+            _StackTotalTickets.pop();
+        }
+        cout << endl;
+    }
+
+    void PrintAllTickets()
+    {
+        queue<clsTicket> Temp = QueueLine;
+
+        cout << "\n\t\t\t\t" << "         --Tickets-- ";
+
+        if(Temp.empty())
+        {
+            cout<<": No Tickets."<<endl;
+            return;
         }
         
-        void PrintInfo()
+        while (!Temp.empty())
         {
-            
-            cout<<"\t\t\t\t__________________________________\n"<<endl;
-            cout<<"\t\t\t\t            Queue Info            "<<endl;
-            cout<<"\t\t\t\t__________________________________\n"<<endl;
-            cout<<_Taps<<"Prefix = "<<_Prefix<<endl;
-            cout<<_Taps<<"Total Tickets = "<<_TotalTickets.size()<<endl;
-            cout<<_Taps<<"Served Clients = "<<_ServedClients<<endl;
-            cout<<_Taps<<"Wating Clients = "<<_WatingClients<<endl;
-            cout<<"\t\t\t\t__________________________________\n"<<endl;
+            Temp.front().Print();
+            Temp.pop();
         }
+    }
 
-        void PrintTicketsLineRTL()
-        {
-            cout<<"\t\tTickets :  ";
-            queue<TicketInfo> _tempTotalTickets=_TotalTickets;
-            while(!_tempTotalTickets.empty())
-            {
-                cout<<_tempTotalTickets.front().PrefixNumber<<" <-- ";
-                _tempTotalTickets.pop();
-            }
-            cout<<endl;
-        }
+    bool _ServeNextclient()
+    {
+        if(QueueLine.empty())
+            return false;
 
-        void PrintTicketsLineLTR()
-        {
-            cout<<"\t\tTickets :  ";
-            stack<TicketInfo> _StackTotalTickets=_ConvertFromQueueToStack(_TotalTickets);
-            while(!_StackTotalTickets.empty())
-            {
-                cout<<_StackTotalTickets.top().PrefixNumber<<" --> ";
-                _StackTotalTickets.pop();
-            }
-            cout<<endl;
-        }
-
-        void PrintAllTickets()
-        {
-            queue<TicketInfo> Temp=_TotalTickets;
-
-            cout<<_Taps<<"      --Tickets--"<<endl;
-
-            while(!Temp.empty())
-            {
-                cout<<"\t\t\t\t___________________________________\n"<<endl;
-                cout<<_Taps<<"\t"<<Temp.front().PrefixNumber<<endl; 
-                cout<<"\n"<<_Taps;
-                Temp.front().TicketDate.Print();  //Date
-                cout<<" - "<<Temp.front().TicketTime.TimeToString()<<endl;  //time
-                cout<<_Taps<<"Wating Clients = "<<Temp.front().WatingClients<<endl;
-                cout<<_Taps<<"  Serve Time In "<<endl;
-                cout<<_Taps<<"  "<<Temp.front().ServeTime<<"."<<endl;
-                cout<<"\t\t\t\t___________________________________\n"<<endl;
-                
-                Temp.pop();
-            }
-        }
-
-        void _ServeNextclient()
-        {
-            _ServedClients++;
-            _WatingClients--;
-        }
+        QueueLine.pop();
+        return true;
+    }
 };
