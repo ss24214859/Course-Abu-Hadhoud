f4ac35a56d5b946b9025d8495e16c13d380da908
Hamo Shaaban <ss24214859@gmail.com>
2025-08-16 01:49:59 +0300

    finsh Course 13 and start to Course 14

---
+++ 14_C#_Level_1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//ProgrammingAdvices.com
+//Mohammed Abu-Hadhoud
+
+using System;
+
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine(rnd); // returns random integers >= 10 and < 20
+            }
+        }
+    }
+}
+
