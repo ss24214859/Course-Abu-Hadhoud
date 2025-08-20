b75b93a9210fa41774a6177f940f7d9534e6cf50
Hamo Shaaban <ss24214859@gmail.com>
2025-08-20 19:31:00 +0300

    Picture Box Exercise  Solution

---
+++ 14_C#_Level_1/MyWindowsApplications/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbBoy = new System.Windows.Forms.RadioButton();
+            this.rbGirl = new System.Windows.Forms.RadioButton();
+            this.rbPen = new System.Windows.Forms.RadioButton();
+            this.rbBook = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbBoy
+            // 
+            this.rbBoy.AutoSize = true;
+            this.rbBoy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbBoy.Location = new System.Drawing.Point(170, 429);
+            this.rbBoy.Name = "rbBoy";
+            this.rbBoy.Size = new System.Drawing.Size(62, 26);
+            this.rbBoy.TabIndex = 1;
+            this.rbBoy.Tag = "Boy";
+            this.rbBoy.Text = "Boy";
+            this.rbBoy.UseVisualStyleBackColor = true;
+            this.rbBoy.CheckedChanged += new System.EventHandler(this.rbBoy_CheckedChanged);
+            // 
+            // rbGirl
+            // 
+            this.rbGirl.AutoSize = true;
+            this.rbGirl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbGirl.Location = new System.Drawing.Point(301, 429);
+            this.rbGirl.Name = "rbGirl";
+            this.rbGirl.Size = new System.Drawing.Size(59, 26);
+            this.rbGirl.TabIndex = 2;
+            this.rbGirl.Tag = "Girl";
+            this.rbGirl.Text = "Girl";
+            this.rbGirl.UseVisualStyleBackColor = true;
+            this.rbGirl.CheckedChanged += new System.EventHandler(this.rbGirl_CheckedChanged);
+            // 
+            // rbPen
+            // 
+            this.rbPen.AutoSize = true;
+            this.rbPen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbPen.Location = new System.Drawing.Point(426, 429);
+            this.rbPen.Name = "rbPen";
+            this.rbPen.Size = new System.Drawing.Size(63, 26);
+            this.rbPen.TabIndex = 3;
+            this.rbPen.Tag = "Pen";
+            this.rbPen.Text = "Pen";
+            this.rbPen.UseVisualStyleBackColor = true;
+            this.rbPen.CheckedChanged += new System.EventHandler(this.rbPen_CheckedChanged);
+            // 
+            // rbBook
+            // 
+            this.rbBook.AutoSize = true;
+            this.rbBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbBook.Location = new System.Drawing.Point(574, 429);
+            this.rbBook.Name = "rbBook";
+            this.rbBook.Size = new System.Drawing.Size(72, 26);
+            this.rbBook.TabIndex = 4;
+            this.rbBook.Tag = "Book";
+            this.rbBook.Text = "Book";
+            this.rbBook.UseVisualStyleBackColor = true;
+            this.rbBook.CheckedChanged += new System.EventHandler(this.rbBook_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Girl;
+            this.pictureBox1.Location = new System.Drawing.Point(147, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(508, 304);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Showcard Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.ForeColor = System.Drawing.Color.Red;
+            this.lbTitle.Location = new System.Drawing.Point(336, 20);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(121, 46);
+            this.lbTitle.TabIndex = 5;
+            this.lbTitle.Text = "Title";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(859, 513);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.rbBook);
+            this.Controls.Add(this.rbPen);
+            this.Controls.Add(this.rbGirl);
+            this.Controls.Add(this.rbBoy);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton rbBoy;
+        private System.Windows.Forms.RadioButton rbGirl;
+        private System.Windows.Forms.RadioButton rbPen;
+        private System.Windows.Forms.RadioButton rbBook;
+        private System.Windows.Forms.Label lbTitle;
+    }
+}
+
---
+++ 14_C#_Level_1/MyWindowsApplications/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Properties;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rbBoy_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resources.Boy;
+            lbTitle.Text = ((RadioButton)sender).Tag.ToString();
+        }
+
+        private void rbGirl_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resources.Girl;
+            lbTitle.Text = ((RadioButton)sender).Tag.ToString();
+        }
+
+        private void rbPen_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resources.Pen;
+            lbTitle.Text = ((RadioButton)sender).Tag.ToString();
+        }
+
+        private void rbBook_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resources.Book;
+            lbTitle.Text = ((RadioButton)sender).Tag.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbBoy.Checked = true;
+        }
+    }
+}
