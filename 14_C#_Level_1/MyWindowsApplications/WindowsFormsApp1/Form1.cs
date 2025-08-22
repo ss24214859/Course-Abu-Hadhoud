57f788225d6700a9958cf1bc527bab0bc9085fac
Hamo Shaaban <ss24214859@gmail.com>
2025-08-22 09:01:10 +0300

    Applaction for PictureBox

--- 14_C#_Level_1/MyWindowsApplications/WindowsFormsApp1/Form1.cs
+++ 14_C#_Level_1/MyWindowsApplications/WindowsFormsApp1/Form1.cs
@@ -17,10 +17,35 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-
+        void UpadateRbOption(object sender)
+        {
+            RadioButton rbSelected = (RadioButton)sender;
+            if(rbSelected.Checked)
+            {
+                switch (rbSelected.Name)
+                {
+                    case  "rbBoy":
+                        pictureBox1.Image = Resources.Boy;
+                        break;
+                    case "rbGirl":
+                        pictureBox1.Image = Resources.Girl;
+                        break;
+                    case "rbPen":
+                        pictureBox1.Image = Resources.Pen;
+                        break;
+                    case "rbBook":
+                        pictureBox1.Image = Resources.Book;
+                        break;
+                }
+                lbTitle.Text = rbSelected.Tag.ToString();
+            }
+           
+        }
         private void rbBoy_CheckedChanged(object sender, EventArgs e)
         {
             pictureBox1.Image = Resources.Boy;
+            
+
             lbTitle.Text = ((RadioButton)sender).Tag.ToString();
         }
 
