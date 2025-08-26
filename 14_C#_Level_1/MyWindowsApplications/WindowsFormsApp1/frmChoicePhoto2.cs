59cd12ef7181ed51ab6338860832a20d2a0a75ea
Hamo Shaaban <ss24214859@gmail.com>
2025-08-26 22:05:38 +0300

    the shortcut Slution for frmChoicePhoto2

--- 14_C#_Level_1/MyWindowsApplications/WindowsFormsApp1/frmChoicePhoto2.cs
+++ 14_C#_Level_1/MyWindowsApplications/WindowsFormsApp1/frmChoicePhoto2.cs
@@ -25,25 +25,30 @@ namespace WindowsFormsApp1
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            switch(cboChoicePhoto.SelectedIndex)
-            {
-                case 0:
-                    lbTitle.Text = "Book";
-                    pictureBox1.Image = Resources.Book;
-                    break;
-                case 1:
-                    lbTitle.Text = "Boy";
-                    pictureBox1.Image = Resources.Boy;
-                    break;
-                case 2:
-                    lbTitle.Text = "Girl";
-                    pictureBox1.Image = Resources.Girl;
-                    break;
-                case 3:
-                    lbTitle.Text = "Pen";
-                    pictureBox1.Image = Resources.Pen;
-                    break;
-            }
+            //switch(cboChoicePhoto.SelectedIndex)
+            //{
+            //    case 0:
+            //        lbTitle.Text = "Book";
+            //        pictureBox1.Image = Resources.Book;
+            //        break;
+            //    case 1:
+            //        lbTitle.Text = "Boy";
+            //        pictureBox1.Image = Resources.Boy;
+            //        break;
+            //    case 2:
+            //        lbTitle.Text = "Girl";
+            //        pictureBox1.Image = Resources.Girl;
+            //        break;
+            //    case 3:
+            //        lbTitle.Text = "Pen";
+            //        pictureBox1.Image = Resources.Pen;
+            //        break;
+            //}
+
+            //this is the shortcut Slution.
+            string ActiveChoice = cboChoicePhoto.Text;
+            lbTitle.Text = ActiveChoice;
+            pictureBox1.Image =(Image )Resources.ResourceManager.GetObject(ActiveChoice);
 
         }
 
